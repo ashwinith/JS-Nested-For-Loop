260fa100845ec8a75cde74f59dc640368241ba81
ashwinith <ashwinith@gmail.com>
2019-10-10 23:44:09 +0530

    JS Nested For Loop Pattern

---
+++ NestedForLoop.js
@@ -0,0 +1,28 @@
+// Nested For Loop Task 1
+
+document.write("Task 1 </br>");
+
+for(let counter1=0;counter1<4;counter1++)
+{
+    for(let counter2=0; counter2<4;counter2++)
+    {
+        document.write(" * ");
+    }
+    document.write("</br>");
+}
+
+
+
+// Nested For Loop Task 2
+
+document.write("Task 2 </br>");
+
+for (let i=0;i<4;i++)
+{
+    for(let j=4;i<j;j--)
+    {
+        document.write(" * ");
+
+    }
+    document.write("</br>");
+}
